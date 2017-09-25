a52ec060e31e0dc94ec710035a8404a74a1eaf7e
Hyungjun Chae <hyungjun.chae@g.softbank.co.jp>
2017-09-26 01:15:19 +0900

    README作成

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -2,6 +2,20 @@
 var Index = new Vue({
   el: "#app",
   data: {
-   title: "hogehoge"       
+   title: "hogehoge",      
+   soojin: "kkk",
+   hyungjun: "ccc"
+  },
+  mounted() {
+    console.log("created");
+  },
+  methods: {
+    test () {
+      alert(this.soojin);
+    },
+
+    test2 () {
+      alert(this.hyungjun);
+    }
   }
 });
